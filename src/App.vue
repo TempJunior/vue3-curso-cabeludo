3bce80a54b8bb8ea71ebecced8ac91c98c0ce714
TempJunior <junior_sadaanzinhoa@hotmail.com>
2025-03-05 17:39:53 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+<BannerPika/>
+<MainContent/>
+
+</template>
+
+<style scoped>
+
+</style>
+
+<script lang="ts">
+import BannerPika from '@/components/BannerPika.vue'
+import MainContent from '@/components/MainContent.vue'
+
+export default {
+  components: {
+    MainContent,
+    BannerPika
+  }
+}
+</script>
---
+++ src/components/BannerPika.vue
@@ -0,0 +1,70 @@
+<template>
+  <header class="banner">
+    <div class="apresentacao">
+      <img src="/src/assets/imagens/logo.svg" alt="" class="logo" />
+
+      <p class="cabecalho-lg frase-cabecalho">
+        <span class="texto-verde">Um banquete para esse</span>
+        Cabeludo boiola
+      </p>
+
+      <p class="subtitulo-lg">Esse cabeludo é muito bixooona</p>
+    </div>
+
+    <img src="/src/assets/imagens/foto-banner.png" alt="" class="foto-banner" />
+  </header>
+</template>
+
+<style scoped>
+.banner {
+  padding: 4rem 7.5rem;
+  color: var(--creme);
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  column-gap: 3.25rem;
+}
+
+.logo {
+  height: 4.5rem;
+  margin-bottom: 3rem;
+}
+
+.frase-cabecalho {
+  margin-bottom: 2rem;
+}
+
+.texto-verde {
+  color: var(--verde-medio, #3d6d4a);
+}
+
+.foto-banner {
+  width: 35rem;
+}
+
+@media only screen and (max-width: 1300px) {
+  .banner {
+    padding: 4rem 3.75rem;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+  }
+
+  .logo {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+  }
+}
+
+@media only screen and (max-width: 767px) {
+  .banner {
+    padding: 4rem 1.5rem;
+  }
+
+  .foto-banner {
+    width: min(100%, 21.25rem);
+  }
+}
+</style>
